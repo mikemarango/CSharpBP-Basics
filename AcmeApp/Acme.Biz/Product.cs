44b5be6fddf78b9bbfd33d4af1e2ff1525f970e2
Michael Marango <mike.marango@gmail.com>
2016-02-09 03:35:34 -0800

    Defining contructors

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    public class Product
+    {
+        public Product()
+        {
+            Console.WriteLine("Product instance created");
+        }
+
+        public Product(int productId, string productName, string productDescription) : this()
+        {
+            _productId = productId;
+            _productName = productName;
+            _productDescription = productDescription;
+            
+            Console.WriteLine($"Product instance has a name: {ProductName}");
+        }
+
+        private string _productName;
+        public string ProductName
+        {
+            get { return _productName; }
+            set { _productName = value; }
+        }
+
+        private string _productDescription;
+        public string ProductDescription
+        {
+            get { return _productDescription; }
+            set { _productDescription = value; }
+        }
+
+        private int _productId;
+        public int ProductId
+        {
+            get { return _productId; }
+            set { _productId = value; }
+        }
+
+        public string SayHello()
+        {
+            return $"Hello {ProductName} ({ProductId}): {ProductDescription}";
+        }
+    }
+}
